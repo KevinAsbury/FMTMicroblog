db4cd2f966fa872b16b90657742dc86548b105e1
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-01-24 12:08:16 -0500

    define post model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -14,3 +15,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, body, timestamp, user_id):
+        body = body
+        timestamp = timestamp
+        user_id = user_id
+
+    def __repr__(self):
+        return f'<Post {body}>'
