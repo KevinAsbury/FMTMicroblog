722421f6d2824f438d6dd5542889aef735e397b1
Kevin Asbury <kevinasbury.dev@gmail.com>
2020-01-24 10:00:03 -0500

    Mock user and html page

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,14 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    user = {'username': 'Kevin'} # Mock user
+    # Mock html page
+    return '''
+        <html>
+            <head>
+            <body>
+                <h1>Hello, ''' + user['username'] +  '''!</h1>
+            </body>
+            </head>
+        </html>
+    '''
